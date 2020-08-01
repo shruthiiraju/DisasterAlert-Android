71f20a0706d15e890aee1cd27321158a04a7141c
Andrew Abishek Azhagesan <44633226+AndyGamer3699@users.noreply.github.com>
2020-08-01 09:15:41 +0530

    map updates

--- app/src/main/java/com/example/disasteralert/DisasterActivity.java
+++ app/src/main/java/com/example/disasteralert/DisasterActivity.java
@@ -1,10 +1,20 @@
 package com.example.disasteralert;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,18 +23,50 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import static com.example.disasteralert.MainActivity.FASTEST_UPDATE_INTERVAL;
+import static com.example.disasteralert.MainActivity.MAX_UPDATE_INTERVAL;
+import static com.example.disasteralert.MainActivity.UPDATE_INTERVAL;
 
 public class DisasterActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    private Location location;
 
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disaster);
 
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationRequest.setMaxWaitTime(MAX_UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest,
+                new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        location = locationResult.getLastLocation();
+                        updateMap();
+                    }
+                },
+                DisasterActivity.this.getMainLooper()
+        );
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        assert  mapFragment != null;
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
         //Initialise the content
@@ -43,6 +85,17 @@ public class DisasterActivity extends FragmentActivity implements OnMapReadyCall
 
     }
 
+    private void updateMap() {
+        LatLng location = new LatLng(this.location.getLatitude(), this.location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(location)
+                .zoom(12)
+                .bearing(90)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
     private void updateHeatMap() {
     }
 
@@ -52,15 +105,6 @@ public class DisasterActivity extends FragmentActivity implements OnMapReadyCall
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng location = MainActivity.locations.myCoordinates;
         mMap = googleMap;
-        //mMap.addMarker(new MarkerOptions().position(location).title("default"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(location)
-                .zoom(12)
-                .bearing(90)
-                .build();
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
